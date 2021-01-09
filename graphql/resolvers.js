6c81ea1fa06a1b261e1e4043bc36f48814d3177b
wyjeon <wyjeon.info@gmail.com>
2021-01-09 14:41:40 +0900

    Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getById, getMovies, addMovie } from "./db";
+import { getById, getMovies, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
@@ -6,7 +6,8 @@ const resolvers = {
     movie: (_, { id }) => getById(id)
   },
   Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score)
+    addMovie: (_, {name, score}) => addMovie(name, score),
+    deleteMovie: (_, {id}) => deleteMovie(id)
   }
 }
 
