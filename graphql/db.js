2360c095101fe8136f98df1b235b321cb32f9d5a
wyjeon <wyjeon.info@gmail.com>
2021-01-09 15:13:28 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,16 @@
-let movies = [
-  {
-    id: 0,
-    name: "Spider man",
-    score: 100
-  },
-  {
-    id: 1,
-    name: "Iron man",
-    score: 70
-  },
-  {
-    id: 2,
-    name: "Thor",
-    score: 60
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-}
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id != id);
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length+1}`,
-    name,
-    score
-  }
-  movies.push(newMovie);
-  return newMovie;
-}
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,9 @@
-import { getById, getMovies, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score),
-    deleteMovie: (_, {id}) => deleteMovie(id)
+    movies: (_, { rating, limit }) => getMovies(limit, rating)
   }
-}
+};
 
 export default resolvers;
